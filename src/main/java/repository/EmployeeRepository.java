a32ae7b1b7927e7c38451d9156bfaad625877e1a
kage1011 <khaivv123@gmail.com>
2021-09-13 21:18:48 +0700

    repo branch

--- src/main/java/repository/EmployeeRepository.java
+++ src/main/java/repository/EmployeeRepository.java
@@ -1,10 +1,36 @@
 package repository;
 
 import entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+import javax.transaction.Transactional;
+import java.util.List;
 
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Query(value = "select * from employee", nativeQuery = true)
+    List<Employee> getAll();
+
+    @Query(value = "select * from employee where firstname = ?1", nativeQuery = true)
+    List<Employee> getListByFirstName(String name);
+
+    @Transactional
+    @Modifying
+    @Query(value = "insert into employee(firstname) values(?1)", nativeQuery = true)
+    void addEmployee(String name);
+
+    @Transactional
+    @Modifying
+    @Query(value = "update employee set firstname = ?2 where id = ?1", nativeQuery = true)
+    void updateEmployee(Integer id, String firstname);
+
+    @Transactional
+    @Modifying
+    @Query(value = "delete from employee where id = ?1", nativeQuery = true)
+    void deleteEmployeeById(Integer id);
 }
--- src/test/java/EmployeeRepositoryTest.java
+++ src/test/java/EmployeeRepositoryTest.java
@@ -1,5 +1,35 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import repository.EmployeeRepository;
 
 @SpringBootTest
 public class EmployeeRepositoryTest {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Test
+    public void getAll() {
+
+        System.out.println("getAll: " + employeeRepository.getAll());
+        System.out.println("getListByName: " + employeeRepository.getListByFirstName("hanh"));
+    }
+
+    @Test
+    public void add() {
+
+        employeeRepository.addEmployee("khai");
+    }
+
+    @Test
+    public void update() {
+
+        employeeRepository.updateEmployee(1, "khai1");
+    }
+
+    @Test
+    public void delete() {
+
+        employeeRepository.deleteEmployeeById(1);
+    }
 }
